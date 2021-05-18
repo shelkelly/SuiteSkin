2aaf69e68a018a212b1ef280fac75b5041a5903d
Shelby <shelbypkelly@gmail.com>
2021-05-17 22:48:41 -0400

    images and subtext added to carousel. slightly styled. needs more styling

--- suite-skin/src/components/Carousel.js
+++ suite-skin/src/components/Carousel.js
@@ -2,27 +2,48 @@ import React from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 
-const responsive = {
-    0: { items: 1 },
-    568: { items: 2 },
-    1024: { items: 3 },
+const items = [
+    <div className="item" data-value="1"><img src="https://github.com/shelkelly/SuiteSkin/blob/main/suite-skin/src/images/beforeandafters/baa1.png?raw=true" class="carouselPhoto" alt="Before and After Photo"></img><p>Acne Treatment</p></div>,
+    <div className="item" data-value="2"><img src="https://github.com/shelkelly/SuiteSkin/blob/main/suite-skin/src/images/beforeandafters/baa2.png?raw=true" class="carouselPhoto" alt="Before and After Photo"></img><p>Acne Treatment</p></div>,
+    <div className="item" data-value="3"><img src="https://github.com/shelkelly/SuiteSkin/blob/main/suite-skin/src/images/beforeandafters/baa3.png?raw=true" class="carouselPhoto" alt="Before and After Photo"></img><p>Lash Lift and Tint</p></div>,
+    <div className="item" data-value="4"><img src="https://github.com/shelkelly/SuiteSkin/blob/main/suite-skin/src/images/beforeandafters/baa4.png?raw=true" class="carouselPhoto" alt="Before and After Photo"></img><p>Brow Shaping</p></div>,
+    <div className="item" data-value="5"><img src="https://github.com/shelkelly/SuiteSkin/blob/main/suite-skin/src/images/beforeandafters/baa5.png?raw=true" class="carouselPhoto" alt="Before and After Photo"></img><p>Lash Lift</p></div>,
+    <div className="item" data-value="6"><img src="https://github.com/shelkelly/SuiteSkin/blob/main/suite-skin/src/images/beforeandafters/baa6.png?raw=true" class="carouselPhoto" alt="Before and After Photo"></img><p>Lash Lift and Tint</p></div>,
+    <div className="item" data-value="7"><img src="https://github.com/shelkelly/SuiteSkin/blob/main/suite-skin/src/images/beforeandafters/baa7.png?raw=true" class="carouselPhoto" alt="Before and After Photo"></img><p>Lash Lift</p></div>,
+    <div className="item" data-value="8"><img src="https://github.com/shelkelly/SuiteSkin/blob/main/suite-skin/src/images/beforeandafters/baa8.png?raw=true" class="carouselPhoto" alt="Before and After Photo"></img><p>Brow Shaping</p></div>,
+    <div className="item" data-value="9"><img src="https://github.com/shelkelly/SuiteSkin/blob/main/suite-skin/src/images/beforeandafters/baa9.png?raw=true" class="carouselPhoto" alt="Before and After Photo"></img><p>Lash Lift</p></div>
+];
+
+const renderSlideInfo = ({ item, itemsCount }) => {
+    return `${item}\\${itemsCount}`;
 };
 
-const items = [
-    <div className="item" data-value="1">heyyy</div>,
-    <div className="item" data-value="2">2</div>,
-    <div className="item" data-value="3">3</div>,
-    <div className="item" data-value="4">4</div>,
-    <div className="item" data-value="5">5</div>,
-];
+const renderDotsItem = ({ isActive }) => {
+    return isActive ? 'x' : 'o';
+};
+
+const renderPrevButton = ({ isDisabled }) => {
+    return <span style={{ opacity: isDisabled ? '0.5' : 1 }}>&lt;</span>;
+};
+
+const renderNextButton = ({ isDisabled }) => {
+    return <span style={{ opacity: isDisabled ? '0.5' : 1 }}>&gt;</span>;
+};
+
+const renderPlayPauseButton = ({ isPlaying }) => {
+    return isPlaying ? 'PAUSE' : 'PLAY';
+};
 
 const Carousel = () => (
     <AliceCarousel
         mouseTracking
         items={items}
-        responsive={responsive}
-        controlsStrategy="alternate"
+        autoPlayControls
+        disableSlideInfo={false}
+        renderSlideInfo={renderSlideInfo}
+        renderDotsItem={renderDotsItem}
+        renderPrevButton={renderPrevButton}
+        renderNextButton={renderNextButton}
     />
 );
-
 export default Carousel;
